834bcfc4eec72610468a10f3ac57468e00d26392
Yuriko Anzai Schumacher <yuriko.schumacher@gmail.com>
2021-04-17 19:40:12 -0400

    add responsivity

--- scripts/script.js
+++ scripts/script.js
@@ -57,13 +57,18 @@ let currentStep = "step-1";
 let nextStep = "step-2";
 
 const dotsG = d3.select(".time-container");
-const videoG = d3.select("#video").append("g").classed("video-container", true);
+const video = d3.select("#video");
+console.log(windowH);
+video
+	.style("top", window > 576 ? `${0.17 * windowH}px` : "100px")
+	.style("left", window > 576 ? "50px" : 0);
+const videoG = video.append("g").classed("video-container", true);
 
 // ---------------- STEP FUNCTION -----------------
 // ---------------- FIRST STEP -----------------
 function firstStep() {
 	skipButton.style("visibility", "hidden");
-	d3.select("#video").classed("video-explore", false);
+	video.classed("video-explore", false);
 	videoG.selectAll("video").remove();
 	dotsG
 		.selectAll("circle")
@@ -75,6 +80,10 @@ function firstStep() {
 // ---------------- MIDDLE STEPS -----------------
 
 function stepFn() {
+	if (windowW <= 576) {
+		d3.select("#chart").style("opacity", 1);
+		d3.select("#map").style("opacity", 1);
+	}
 	skipButton.style("visibility", "visible");
 	dotsG
 		.selectAll("circle")
@@ -115,7 +124,7 @@ function stepFn() {
 		.attr("fill-opacity", 0.3);
 
 	// ---------------- ADD AND REMOVE VIDEOS -----------------
-	d3.select("#video").classed("video-explore", false);
+	video.classed("video-explore", false);
 	videoG.selectAll("video").remove();
 	videoG.style("opacity", 0);
 
@@ -128,7 +137,7 @@ function stepFn() {
 			.append("video")
 			.attr("controls", true)
 			.attr("muted", true)
-			.attr("width", 0.45 * windowW);
+			.attr("width", windowW > 576 ? 0.45 * windowW : windowW - 20);
 		videoEl
 			.append("source")
 			.attr("src", videoToAdd[0].src)
@@ -139,75 +148,44 @@ function stepFn() {
 // ------------------- FOR LAST STEP --------------------
 
 function exploreMoreVideos() {
-	skipButton.style("visibility", "hidden");
-	d3.select("#step-15").select(".dark").style("padding", 0);
-	d3.select("#chart").style("z-index", 99);
-	dotsG
-		.selectAll("circle")
-		.classed("active-circles", false)
-		.attr("r", 5)
-		.attr("fill-opacity", 0.3)
-		.attr("pointer-events", "auto");
-	let dots = dotsG.selectAll("circle");
-	dots.style("cursor", "pointer");
-	let step14 = d3.select("#step-14").select("div");
-	let isClicked = false;
-	let prevDot;
+	if (windowW <= 576) {
+		d3.select("#chart").style("opacity", 0);
+		d3.select("#map").style("opacity", 0);
+		d3.select("#step-14").style("display", "none");
+		d3.select("#step-15").style("display", "none");
+	} else {
+		skipButton.style("visibility", "hidden");
+		d3.select("#step-15").select(".dark").style("padding", 0);
+		d3.select("#chart").style("z-index", 99);
+		dotsG
+			.selectAll("circle")
+			.classed("active-circles", false)
+			.attr("r", 5)
+			.attr("fill-opacity", 0.3)
+			.attr("pointer-events", "auto");
+		let dots = dotsG.selectAll("circle");
+		dots.style("cursor", "pointer");
+		let step14 = d3.select("#step-14").select("div");
+		let isClicked = false;
+		let prevDot;
 
-	d3.select("#video").classed("video-explore", true);
+		video.classed("video-explore", true);
 
-	dots.on("mouseover", function (e, d) {
-		isClicked = false;
-		videoG.selectAll("video").remove();
-		if (prevDot !== undefined) {
-			prevDot
-				.attr("r", 5)
-				.attr("fill-opacity", 0.3)
-				.classed("dot-active", false);
-			if (prevDot.category === "BPD Tweets/News") {
-				return;
-			} else {
-				let prevDotId = prevDot.attr("id");
-				map.setPaintProperty(prevDotId, "circle-opacity", 0);
-			}
-		}
-		let dot = d3.select(this);
-		dot.attr("r", 7).attr("fill-opacity", 1).classed("dot-active", true);
-		step14
-			.classed("tooltip", true)
-			.html(
-				`Time: <b>${d3.timeFormat("%I:%M %p")(
-					d.parsedTime
-				)}</b><br>Location: <b>${d.location}</b><br><p>${
-					d.comment
-				}.</p>`
-			);
-		if (d.category === "BPD Tweets/News") {
-			return;
-		} else {
-			let id = dot.attr("id");
-			map.setPaintProperty(id, "circle-opacity", 1);
-		}
-	})
-		.on("mouseout", function (e, d) {
-			if (!isClicked) {
-				d3.select(this)
+		dots.on("mouseover", function (e, d) {
+			isClicked = false;
+			videoG.selectAll("video").remove();
+			if (prevDot !== undefined) {
+				prevDot
 					.attr("r", 5)
 					.attr("fill-opacity", 0.3)
 					.classed("dot-active", false);
-				step14.html(
-					"<p>Click time stamps above to watch more video footage from the night.</p>"
-				);
-				if (d.category === "BPD Tweets/News") {
+				if (prevDot.category === "BPD Tweets/News") {
 					return;
 				} else {
-					let id = d3.select(this).attr("id");
-					map.setPaintProperty(id, "circle-opacity", 0);
+					let prevDotId = prevDot.attr("id");
+					map.setPaintProperty(prevDotId, "circle-opacity", 0);
 				}
 			}
-		})
-		.on("click", function (e, d) {
-			isClicked = !isClicked;
 			let dot = d3.select(this);
 			dot.attr("r", 7)
 				.attr("fill-opacity", 1)
@@ -227,25 +205,65 @@ function exploreMoreVideos() {
 				let id = dot.attr("id");
 				map.setPaintProperty(id, "circle-opacity", 1);
 			}
-			prevDot = dotsG.select(".dot-active");
-			// ---------------- ADD AND REMOVE VIDEOS -----------------
+		})
+			.on("mouseout", function (e, d) {
+				if (!isClicked) {
+					d3.select(this)
+						.attr("r", 5)
+						.attr("fill-opacity", 0.3)
+						.classed("dot-active", false);
+					step14.html(
+						"<p>Click time stamps above to watch more video footage from the night.</p>"
+					);
+					if (d.category === "BPD Tweets/News") {
+						return;
+					} else {
+						let id = d3.select(this).attr("id");
+						map.setPaintProperty(id, "circle-opacity", 0);
+					}
+				}
+			})
+			.on("click", function (e, d) {
+				isClicked = !isClicked;
+				let dot = d3.select(this);
+				dot.attr("r", 7)
+					.attr("fill-opacity", 1)
+					.classed("dot-active", true);
+				step14
+					.classed("tooltip", true)
+					.html(
+						`Time: <b>${d3.timeFormat("%I:%M %p")(
+							d.parsedTime
+						)}</b><br>Location: <b>${d.location}</b><br><p>${
+							d.comment
+						}.</p>`
+					);
+				if (d.category === "BPD Tweets/News") {
+					return;
+				} else {
+					let id = dot.attr("id");
+					map.setPaintProperty(id, "circle-opacity", 1);
+				}
+				prevDot = dotsG.select(".dot-active");
+				// ---------------- ADD AND REMOVE VIDEOS -----------------
 
-			videoG.selectAll("video").remove();
-			videoG.style("opacity", 0);
+				videoG.selectAll("video").remove();
+				videoG.style("opacity", 0);
 
-			if (d.category === "Bodycam footage") {
-				videoG.transition().duration(2000).style("opacity", 1);
-				let videoEl = videoG
-					.append("video")
-					.attr("controls", true)
-					.attr("muted", true)
-					.attr("width", windowW < 1500 ? 600 : 800);
-				videoEl
-					.append("source")
-					.attr("src", d.src)
-					.attr("type", "video/mp4");
-			}
-		});
+				if (d.category === "Bodycam footage") {
+					videoG.transition().duration(2000).style("opacity", 1);
+					let videoEl = videoG
+						.append("video")
+						.attr("controls", true)
+						.attr("muted", true)
+						.attr("width", windowW < 1500 ? 600 : 800);
+					videoEl
+						.append("source")
+						.attr("src", d.src)
+						.attr("type", "video/mp4");
+				}
+			});
+	}
 }
 
 function dummyStep() {
